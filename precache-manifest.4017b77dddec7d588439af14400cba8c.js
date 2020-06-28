bfb77b8919a91fcb15ac7f8f2921b39d4f2bd861
Karno Sose <karnosose@gmail.com>
2020-06-28 22:28:09 +0400

    Updates

---
+++ precache-manifest.4017b77dddec7d588439af14400cba8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e22376636faf7ce420e6",
+    "url": "/github-users-list/static/js/runtime~main.e2237663.js"
+  },
+  {
+    "revision": "47303a8dc6e45670f5d3",
+    "url": "/github-users-list/static/js/main.47303a8d.chunk.js"
+  },
+  {
+    "revision": "e62cca13fae3f8cc2388",
+    "url": "/github-users-list/static/js/1.e62cca13.chunk.js"
+  },
+  {
+    "revision": "47303a8dc6e45670f5d3",
+    "url": "/github-users-list/static/css/main.9e7400a7.chunk.css"
+  },
+  {
+    "revision": "9717c9224edbf8007140dc5a6965808c",
+    "url": "/github-users-list/index.html"
+  }
+];
